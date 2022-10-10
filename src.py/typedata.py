29827cf958fd279b5bf98253bbeaf05d544b6a0d
bonidandy <bonidandi@gmail.com>
2022-10-10 10:43:53 +0700

    pert4

---
+++ src.py/typedata.py
@@ -0,0 +1,32 @@
+#string
+a ="hello world"
+print (a)
+print (type(a))
+print("\n")
+
+#int
+b = 12
+print (b)
+print (type(b))
+print("\n")
+
+#float
+c = 10.5
+print(c)
+print(type(c))
+print("\n")
+
+#tuple
+d = 20,5
+print(d)
+print(type(d))
+print("\n")
+
+#datacomplex
+e = 1j
+print(e)
+print(type(e))
+print("\n")
+
+z = complex ('5-9j')
+print (z)
