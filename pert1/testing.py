45e1d2f2b504cd0cd14884bfeb3c80b22ca51aed
bonidandy <bonidandi@gmail.com>
2022-10-10 11:40:12 +0700

    pert4

--- pert1/testing.py
+++ pert1/testing.py
@@ -1,1 +1,51 @@
-print("hello worldd :)")
+#Bonifasius Dandy Krisnanda
+#20210801002
+#Teknik informatika
+
+import numpy as np
+a= np.array (([5, 3],
+             [3, 4]))
+b= np.array (([6, 3],
+             [7, 3]))
+c= np.array (([5,4],
+             [12, 10],
+             [9, 17]))
+print ("A"),
+print (a)
+
+print ("B"),
+print(b)
+
+print ("C")
+print (c)
+
+
+#Perkalian
+print ("a.) A*B")
+e = np.dot(a, b)
+print (e)
+
+#Pengurangan
+print ("b.) A-B")
+e = (a-b)
+print (e)
+
+#perkalian
+print ("c.) A*C")
+e = np.dot(a, c.T)
+print (e)
+
+#perkalian
+print ("d.) B*C")
+e = np.dot (b, c.T)
+print (e)
+
+#pembagian
+print ("d.) B/A")
+print ("invers a")
+e = np.linalg.inv(a)
+print (e)
+print ("    ")
+print ("Hasil B/A")
+d= np.dot(b, e)
+print (d)
---
+++ typedata/typedata.py
@@ -0,0 +1,72 @@
+#string
+a ="hello world"
+print (a)
+print (type(a))
+print("\n")
+
+#int
+b = 12
+print (b)
+print (type(b))
+print("\n")
+
+#float
+c = 10.5
+print(c)
+print(type(c))
+print("\n")
+
+#tuple
+d = 20,5
+print(d)
+print(type(d))
+print("\n")
+
+#datacomplex
+e = 1j
+print(e)
+print(type(e))
+print("\n")
+
+z = complex ('5-9j')
+print (z)
+print("\n")
+
+# type data list
+f = ["a","b","c"]
+print (f)
+print (type(f))
+print("\n")
+
+#contoh 
+list_f = [1,2,3]
+print (list_f[0])
+print("\n")
+
+#contoh tuple
+tuple_f = 1,2,3
+print (tuple_f[0])
+print("\n")
+
+#contoh set
+set_f = {1,2,3}
+print (set_f)
+print("\n")
+
+# type data set
+g = {"a","b","c"}
+print (g)
+print (type(g))
+print("\n")
+
+#type data frozenset
+h = frozenset({1,2,3})
+print (type(h))
+print("\n")
+
+#type data boolean
+i = True
+j = False
+print (i,j)
+print (type(i))
+print (type(j))
